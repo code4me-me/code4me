b42d04abbd806b5414fd37c7184b97126bcae0cb
mrkingmarc01 <marc.jc.otten@gmail.com>
2022-08-17 16:59:08 +0200

    added support for 37 languages and detailed output option and fixed few divby0 errors

--- code4me-server/src/test_eval.py
+++ code4me-server/src/test_eval.py
@@ -2,11 +2,18 @@ import copy
 import json
 import os
 import evaluation
+import sys
 from evaluation import tokenize_code
 
 
 def print_scores(scores):
     size = len(scores)
+
+    if size == 0:
+        print('n = 0')
+        print()
+        return
+
     result = [0, 0, 0, 0, 0, 0, 0]
     for item in scores:
         result[0] += item['bleu']
@@ -47,6 +54,7 @@ def classify_scores(model_data):
     inf_time = []
     token_length = {}
     model_scores = []
+    print_detailed = 'detailed' in sys.argv
 
     for d in model_data:
 
@@ -80,33 +88,39 @@ def classify_scores(model_data):
         else:
             token_length[str(len(tokenized_pred))].append(s)
 
-    print('inf time = ', sum(inf_time) / len(inf_time))
+    if len(inf_time) > 0:
+        print('inf time = ', sum(inf_time) / len(inf_time))
     print_scores(model_scores)
 
-    print('chosen:')
-    print_scores(chosen)
+    if print_detailed:
+        print('chosen:')
+        print_scores(chosen)
 
-    print('not chosen:')
-    print_scores(not_chosen)
+        print('not chosen:')
+        print_scores(not_chosen)
 
-    for i in range(1, 11):
-        if str(i) in token_length:
-            print('token length of prediction = ', i)
-            print_scores(token_length[str(i)])
-            del token_length[str(i)]
-    print('token length of prediction > 10')
-    print_scores(sum(token_length.values(), []))
+        for i in range(1, 11):
+            if str(i) in token_length:
+                print('token length of prediction = ', i)
+                print_scores(token_length[str(i)])
+                del token_length[str(i)]
+        print('token length of prediction > 10')
+        print_scores(sum(token_length.values(), []))
 
-    print('trigger points:')
-    print('manual triggers')
-    print_scores(trigger_points[None])
-    del trigger_points[None]
-    sorted_trigger_points = sorted(trigger_points.items(), key=lambda x: len(x[1]), reverse=True)
-    for index, (tp, tp_scores) in enumerate(sorted_trigger_points):
-        if index >= 10:
-            break
-        print(tp)
-        print_scores(tp_scores)
+        print('trigger points:')
+        print('manual triggers')
+        if None in trigger_points:
+            print_scores(trigger_points[None])
+            del trigger_points[None]
+        else:
+            print('n = 0')
+            print()
+        sorted_trigger_points = sorted(trigger_points.items(), key=lambda x: len(x[1]), reverse=True)
+        for index, (tp, tp_scores) in enumerate(sorted_trigger_points):
+            if index >= 10:
+                break
+            print(tp)
+            print_scores(tp_scores)
 
 
 def classify_all_scores(language_dict):
@@ -151,15 +165,134 @@ def add_data(language_key, d, data):
         unixcoder_list.append(data)
 
 
+def get_language(language):
+    if language == 'python' or language == '.py' or language == 'py':
+        return 'python'
+    elif language == 'java' or language == '.java':
+        return 'java'
+    elif language == 'typescript' or language == '.ts' or language == 'ts':
+        return 'typescript'
+    elif language == 'php' or language == '.php':
+        return 'php'
+    elif language == 'vue':
+        return 'vue'
+    elif language == 'kotlin' or language == 'kt':
+        return 'kotlin'
+    elif language == 'typescriptreact' or language == '.tsx' or language == 'ts' or language == 'typescript jsx':
+        return 'typescriptreact'
+    elif language == 'javascript' or language == '.js' or language == 'js' or language == 'ecmascript 6':
+        return 'javascript'
+    elif language == 'robotframework':
+        return 'robotframework'
+    elif language == 'json' or language == '.json':
+        return 'json'
+    elif language == 'latex':
+        return 'latex'
+    elif language == 'html' or language == '.html':
+        return 'html'
+    elif language == 'javascriptreact' or language == '.jsx' or language == 'jsx':
+        return 'javascriptreact'
+    elif language == 'xml' or language == '.xml':
+        return 'xml'
+    elif language == 'go':
+        return 'go'
+    elif language == 'ruby':
+        return 'ruby'
+    elif language == 'csharp' or language == '.cs' or language == 'c#' or language == 'cs':
+        return 'csharp'
+    elif language == 'blade.php':
+        return 'blade.php'
+    elif language == 'markdown' or language == '.md' or language == 'md':
+        return 'markdown'
+    elif language == 'rust' or language == '.rs' or language == 'rs':
+        return 'rust'
+    elif language == 'css' or language == '.css' or language == 'scss':
+        return 'css'
+    elif language == 'objectivec':
+        return 'objectivec'
+    elif language == 'cpp' or language == '.cpp':
+        return 'cpp'
+    elif language == 'dart' or language == '.dart':
+        return 'dart'
+    elif language == 'sql' or language == '.sql':
+        return 'sql'
+    elif language == '.shellscript' or language == '.sh' or language == 'sh' or language == 'shellscript':
+        return 'shellscript'
+    elif language == 'prisma' or language == '.prisma':
+        return 'prisma'
+    elif language == 'yaml' or language == '.yaml' or language == 'yml' or language == '.yml':
+        return 'yaml'
+    elif language == 'txt' or language == '.txt' or language == 'text' or language == 'plaintext':
+        return 'txt'
+    elif language == 'swift' or language == '.swift':
+        return 'swift'
+    elif language == 'c' or language == '.c':
+        return 'c'
+    elif language == 'gitignore':
+        return 'gitignore'
+    elif language == 'groovy':
+        return 'groovy'
+    elif language == 'perl5':
+        return 'perl5'
+    elif language == 'less':
+        return 'less'
+    elif language == 'scala':
+        return 'scala'
+    elif language == 'julia':
+        return 'julia'
+    else:
+        return 'other'
+
+
 if __name__ == '__main__':
     data_folder = '../data_16_08_2022'
     directory = os.fsencode(data_folder)
     data_dict = {
-        'python': {
+        'python': {},
+        'java': {},
+        'typescript': {},
+        'php': {},
+        'vue': {},
+        'kotlin': {},
+        'typescriptreact': {},
+        'javascript': {},
+        'robotframework': {},
+        'json': {},
+        'latex': {},
+        'html': {},
+        'javascriptreact': {},
+        'xml': {},
+        'go': {},
+        'ruby': {},
+        'csharp': {},
+        'blade.php': {},
+        'markdown': {},
+        'rust': {},
+        'css': {},
+        'objectivec': {},
+        'cpp': {},
+        'dart': {},
+        'sql': {},
+        'shellscript': {},
+        'prisma': {},
+        'yaml': {},
+        'txt': {},
+        'swift': {},
+        'c': {},
+        'gitignore': {},
+        'groovy': {},
+        'perl5': {},
+        'less': {},
+        'scala': {},
+        'julia': {},
+        'other': {}
+    }
+
+    for k in data_dict.keys():
+        data_dict[k] = {
             'incoder': [],
             'unixcoder': []
         }
-    }
 
     for file in os.listdir(directory):
         filename = data_folder + '/' + os.fsdecode(file)
@@ -175,14 +308,11 @@ if __name__ == '__main__':
             if is_not_valid_data(data):
                 continue
 
-            # TODO: add other languages to datadict and if statement and print
-            k = data['language']
-            if k == 'python' or k == '.py' or k == 'py':
-                add_data('python', data_dict, data)
-            else:
-                continue
+            add_data(get_language(data['language']), data_dict, data)
 
-    print('---python---')
-    classify_all_scores(data_dict['python'])
+    data_dict = {k: v for k, v in sorted(data_dict.items(), key=lambda item: len(item[1]['incoder']) + len(item[1]['unixcoder']), reverse=True)}
+    for k in data_dict.keys():
+        print('---', k, '---')
+        classify_all_scores(data_dict[k])
 
     print('done')
