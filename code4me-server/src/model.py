007684cdcb3a22b2b022ecc2f735595b395cf470
Tim van Dam <timvandamcs@gmail.com>
2023-02-07 22:04:49 +0100

    add current codegpt checkpoint to backend api

--- code4me-server/src/model.py
+++ code4me-server/src/model.py
@@ -1,11 +1,12 @@
 from enum import Enum
 import incoder
 import unixcoder_wrapper
-
+import codegpt
 
 class Model(Enum):
     InCoder = (0, incoder.generate)
     UniXCoder = (1, unixcoder_wrapper.generate)
+    CodeGPT = (2, codegpt.codegpt_predict)
 
     @classmethod
     def _missing_(cls, value):
